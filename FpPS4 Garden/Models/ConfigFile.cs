6a90b51a06d0b4a41498b6ee136828aa2e0b5884
Kimie <kimieggie@gmail.com>
2023-08-06 00:48:43 +0300

    config.json and App Folder

---
+++ FpPS4 Garden/Models/ConfigFile.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FpPS4_Garden.Models
+{
+    public class ConfigFile
+    {
+        public string appVer;
+        public string lang;
+    }
+}
