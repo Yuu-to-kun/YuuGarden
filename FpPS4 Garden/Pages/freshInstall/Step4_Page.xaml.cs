b94e8d5157699719b0ede9bd0263a65c4f7f59f7
Mr. Snowy <61592704+MrSn0wy@users.noreply.github.com>
2023-08-07 15:50:07 +0200

    smol ui fixes + Pages with "basic" styling

---
+++ FpPS4 Garden/Pages/freshInstall/Step4_Page.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace FpPS4_Garden.Pages.freshInstall
+{
+    /// <summary>
+    /// Interaction logic for Step4_Page.xaml
+    /// </summary>
+    public partial class Step4_Page : Window
+    {
+        public Step4_Page()
+        {
+            InitializeComponent();
+        }
+    }
+}
