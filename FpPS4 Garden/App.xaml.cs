540f745a4c16a1a3e8bcb8c0907d9181d9f8f1a7
Mr. Snowy <61592704+MrSn0wy@users.noreply.github.com>
2023-08-08 21:24:03 +0200

    Jump List :3

--- FpPS4 Garden/App.xaml.cs
+++ FpPS4 Garden/App.xaml.cs
@@ -10,6 +10,8 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Shell;
+using Windows.Storage;
 
 namespace FpPS4_Garden
 {
@@ -18,8 +20,30 @@ namespace FpPS4_Garden
     /// </summary>
     public partial class App : Application
     {
+        private void InitializeJumpList()
+        {
+            JumpList jumpList = new JumpList();
+
+            JumpTask jumpTask1 = new JumpTask
+            {
+                Title = "Game Compatibility List",
+                Arguments = "https://fpps4.net/compatibility/",
+                CustomCategory = "Links",
+                IconResourcePath = "",
+                ApplicationPath = "https://fpps4.net/compatibility/",
+                Description = "Opens the game compatibility list."
+            };
+
+            jumpList.JumpItems.Add(jumpTask1);
+
+            // Apply changes to the JumpList
+            jumpList.Apply();
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+            InitializeJumpList();
+
             string LocalDataFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "FP_Garden");
             ConfigFunctions configFunctions = new ConfigFunctions();
 
@@ -34,4 +58,7 @@ namespace FpPS4_Garden
             }
         }
     }
+
+
+
 }
